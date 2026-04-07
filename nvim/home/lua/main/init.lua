803600b7383692534ea9fb82c6c9345ef13909d8
2time-b2b <sven.snusberg.dev@gmail.com>
2026-04-07 13:27:58 +0530

    fix: initialize the local plugin file if it does not exist

--- nvim/home/lua/main/init.lua
+++ nvim/home/lua/main/init.lua
@@ -2,8 +2,18 @@ require("main.set")
 require("main.remap")
 require("main.lazy")
 
-os.execute("touch $HOME/.config/nvim/lua/main/lazy/local.lua")
+local local_file = vim.fn.expand("$HOME/.config/nvim/lua/main/lazy/local.lua")
+
+local f = io.open(local_file, "r")
+if f ~= nil then
+  io.close(f)
+else
+  local new = io.open(local_file, "w")
+  if new then
+    new:write("return {}")
+    new:close()
+  end
+end
 
 -- Do not duplicate comments for [Return] and 'O'.
 vim.cmd("autocmd BufEnter * set formatoptions-=ro")
-
